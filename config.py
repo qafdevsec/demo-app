408ed6c641514dcd9eb57159b4fa80e6e77e89de
Leandro M de Moraes <lmmoraes@gmail.com>
2022-09-26 20:08:42 -0300

    debug enabled

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ as env
 import multiprocessing
 
 PORT = int(env.get("PORT", 8080))
-DEBUG_MODE = int(env.get("DEBUG_MODE", 0))
+DEBUG_MODE = int(env.get("DEBUG_MODE", 1))
 
 # Gunicorn config
 bind = ":" + str(PORT)
