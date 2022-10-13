476d8317256680b05343a88eec2116bf4fa6f90b
Leandro M de Moraes <lmmoraes@gmail.com>
2022-10-13 15:42:02 -0300

    fortidevsec config file

--- app.py
+++ app.py
@@ -5,8 +5,5 @@ from app.resources.users import Users, UserLogin, UserByEmail
 from flask import jsonify, render_template
 
 
-
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
